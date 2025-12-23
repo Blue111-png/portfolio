2a89dbf83400ad146e34ef2fd168b98bdd9386eb
Blue <noegl3609@gmail.com>
2025-12-23 13:31:38 +0100

    'fix:images'

--- gallery.js
+++ gallery.js
@@ -1,3 +1,28 @@
+let closebtn=document.getElementById('close-btn')
+let lightbox=document.querySelector('.lightbox')
+let galleryItem = document.querySelectorAll('.gallery img');
+let lightboximg=document.getElementById('lightbox-image')
+
+
+function thumbnail(imageElement){
+  lightbox.style.display='flex'
+  lightboximg.src = imageElement.dataset.large || imageElement.src;
+
+  lightboximg.setAttribute('class', imageElement.className.replace("small", "big"))
+  
+}
+Array.from(galleryItem).forEach(img => {
+  img.addEventListener('click', () => {
+    thumbnail(img);
+  });
+});
+
+closebtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
 
     const scrollBtn=document.querySelector('.scroll')
     scrollBtn.addEventListener('click',()=>{
